cb260a8678b51fadfde5268b1cee7de1620ece1a
aleksandrzaruba1 <156068898+aleksandrzaruba1@users.noreply.github.com>
2024-04-12 15:51:32 +0300

    Update unique_json.h

--- prj.cw/src/utils/unique_json.h
+++ prj.cw/src/utils/unique_json.h
@@ -3,10 +3,16 @@
 #include "jansson.h"
 #include <memory>
 
+/**
+ * @brief Функтор для удаления объекта json_t.
+ */
 struct json_deleter {
-  void operator () (json_t *J) {
-    json_decref(J);
+  void operator () (json_t *J) { /**< Оператор для удаления объекта json_t. */
+    json_decref(J); /**< Уменьшает счетчик ссылок на объект json_t. */
   }
 };
 
+/**
+ * @brief Уникальный указатель на объект json_t с пользовательским удалением.
+ */
 using unique_json = std::unique_ptr<json_t, json_deleter>;
