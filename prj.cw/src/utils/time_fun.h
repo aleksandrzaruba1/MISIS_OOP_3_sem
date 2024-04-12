50835a400d9d4c4d4db1ea7a7f66144a7d1cf74f
aleksandrzaruba1 <156068898+aleksandrzaruba1@users.noreply.github.com>
2024-04-12 15:50:05 +0300

    Update time_fun.h

--- prj.cw/src/utils/time_fun.h
+++ prj.cw/src/utils/time_fun.h
@@ -2,19 +2,53 @@
 #include <iostream>
 #include <ctime>
 
+/**
+ * @brief Функция для создания объекта типа time_t на основе переданных значений года, месяца, дня, часа, минут и секунд.
+ * 
+ * @param y Год.
+ * @param m Месяц.
+ * @param d День.
+ * @param h Час.
+ * @param n Минута.
+ * @param s Секунда.
+ * @return time_t Созданный объект типа time_t.
+ */
 time_t getTime_t(int y, int m, int d, int h, int n, int s);
 
-// Returns 'yyyy-mm-dd_hh:nn:ss'
-extern std::string (*const printDateTimeCsv)(const time_t &t);
+/**
+ * @brief Функция форматирует время, представленное объектом time_t, в строку в формате 'гггг-мм-дд_чч:мм:сс'.
+ * 
+ * @param t Время в формате time_t.
+ * @return std::string Отформатированная строка.
+ */
+std::string printDateTimeCsv(const time_t &t);
 
-// Returns 'yyyy-mm-dd hh:nn:ss'
-extern std::string (*const printDateTimeDb)(const time_t &t);
+/**
+ * @brief Функция форматирует время, представленное объектом time_t, в строку в формате 'гггг-мм-дд чч:мм:сс'.
+ * 
+ * @param t Время в формате time_t.
+ * @return std::string Отформатированная строка.
+ */
+std::string printDateTimeDb(const time_t &t);
 
-// Returns 'yyymmdd_hhnnss'
+/**
+ * @brief Функция форматирует текущее время в строку без разделителей в формате 'ггггммдд_ччммсс'.
+ * 
+ * @return std::string Отформатированная строка.
+ */
 std::string printDateTimeFileName();
 
-// Returns 'mm/dd/yyyy hh:nn:ss'
+/**
+ * @brief Функция форматирует время, представленное объектом time_t, в строку в формате 'мм/дд/гггг чч:мм:сс'.
+ * 
+ * @param t Время в формате time_t.
+ * @return std::string Отформатированная строка.
+ */
 std::string printDateTime(time_t t);
 
-// Returns current 'mm/dd/yyyy hh:mm:ss'
+/**
+ * @brief Функция форматирует текущее время в строку в формате 'мм/дд/гггг чч:мм:сс'.
+ * 
+ * @return std::string Отформатированная строка.
+ */
 std::string printDateTime();
