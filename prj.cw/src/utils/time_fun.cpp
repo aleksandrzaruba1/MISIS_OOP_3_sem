0dcea9452a62713d9178f764ecb644a052c2b6f4
aleksandrzaruba1 <156068898+aleksandrzaruba1@users.noreply.github.com>
2024-04-12 15:43:38 +0300

    Update time_fun.cpp

--- prj.cw/src/utils/time_fun.cpp
+++ prj.cw/src/utils/time_fun.cpp
@@ -1,3 +1,6 @@
+/**
+ * @brief Реализация функций для работы с временем
+ */
 #include <iostream>
 #include <ctime>
 #include "time_fun.h"
@@ -13,13 +16,19 @@ time_t getTime_t(int y, int m, int d, int h, int n, int s) {
   ttm.tm_isdst = -1;
   return mktime(&ttm);
 }
-
+/**
+ * @brief Шаблонная функция для форматирования времени в строку
+ * @tparam fmt Формат времени в виде строки
+ * @param t Время в формате time_t
+ * @return Отформатированная строка времени
+ */
 template <const char *fmt>
 std::string fmtDateTime(const time_t &t) {
   std::string buff(20, '\0');
   buff.resize(strftime(&buff[0], buff.size(), fmt, localtime(&t)));
   return buff;
 }
+// Форматы времени для различных нужд
 
 extern const char csvfmt[] = "%Y-%m-%d_%H:%M:%S";
 const decltype(&fmtDateTime<csvfmt>) printDateTimeCsv = &fmtDateTime<csvfmt>;
@@ -28,7 +37,10 @@ extern const char dbfmt[] = "%Y-%m-%d %H:%M:%S";
 const decltype(&fmtDateTime<dbfmt>) printDateTimeDb = &fmtDateTime<dbfmt>;
 
 extern const char filenamefmt[] = "%Y%m%d_%H%M%S";
-
+/**
+ * @brief Функция для печати времени в формате, используемом в именах файлов
+ * @return Строка с отформатированным временем
+ */
 std::string printDateTimeFileName() {
   return fmtDateTime<filenamefmt>(time(NULL));
 }
