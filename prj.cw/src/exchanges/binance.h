55ec0a3cffac664cfa51ae841b260897aa358217
aleksandrzaruba1 <156068898+aleksandrzaruba1@users.noreply.github.com>
2024-04-12 12:29:14 +0300

    Update binance.h

--- prj.cw/src/exchanges/binance.h
+++ prj.cw/src/exchanges/binance.h
@@ -4,37 +4,75 @@
 
 struct Parameters;
 
+/**
+ * @brief Класс, представляющий биржу Binance.
+ */
 class Binance : public BasicExchange {
 private:
 
 public:
+  /**
+   * @brief Структура, представляющая баланс аккаунта на бирже.
+   */
   struct acct_balance {
-    std::string asset;
-    double free;
-    double locked;
+    std::string asset; /**< Название актива. */
+    double free; /**< Свободный баланс. */
+    double locked; /**< Заблокированный баланс. */
+
+    /**
+     * @brief Конструктор для инициализации баланса аккаунта.
+     * @param a Название актива.
+     * @param f Свободный баланс.
+     * @param l Заблокированный баланс.
+     */
     acct_balance(std::string a, double f, double l)
         : asset(a), free(f), locked(l) {}
   };
 
-  // this is not inheriented since the return type
-  // from each exchange is different
-  std::vector<acct_balance> balances;
+  std::vector<acct_balance> balances; /**< Список балансов аккаунта. */
 
-  // constructor
+  /**
+   * @brief Конструктор по умолчанию.
+   */
   Binance();
 
-  // getter method
-  // return balance of the currency
-  // return -1 if the currency is invalid
+  /**
+   * @brief Метод получения баланса валюты.
+   * @param currency Название валюты.
+   * @return Баланс валюты, -1 если валюта недействительна.
+   */
   double getBalance(std::string &currency) const override;
 
+  /**
+   * @brief Метод получения котировки.
+   * @param params Параметры запроса.
+   * @param symbol Символ.
+   * @return Котировка.
+   */
   quote_t getQuote(Parameters &params, std::string symbol) override;
 
-  // get all available coin & its balance from the api
+  /**
+   * @brief Метод получения доступного баланса аккаунта.
+   * @param params Параметры запроса.
+   */
   void getAvailBalance(Parameters &params) override;
 
+  /**
+   * @brief Метод для аутентифицированного запроса.
+   * @param params Параметры запроса.
+   * @param method Метод запроса.
+   * @param request Запрос.
+   * @param options Опции запроса.
+   * @return Ответ на запрос в формате JSON.
+   */
   json_t *authRequest(Parameters &params, std::string method,
                       std::string request, std::string options) override;
 
+  /**
+   * @brief Метод для получения подписи.
+   * @param params Параметры запроса.
+   * @param payload Нагрузка.
+   * @return Подпись.
+   */
   std::string getSignature(Parameters &params, std::string payload) override;
 };
