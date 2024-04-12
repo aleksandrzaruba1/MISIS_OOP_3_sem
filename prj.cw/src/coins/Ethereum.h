dbe15e3f0ec5d8c83425947f3ab4c0a9e481f8d1
aleksandrzaruba1 <156068898+aleksandrzaruba1@users.noreply.github.com>
2024-04-12 12:05:47 +0300

    Update Ethereum.h

--- prj.cw/src/coins/Ethereum.h
+++ prj.cw/src/coins/Ethereum.h
@@ -1,9 +1,20 @@
 #pragma once
 #include "BasicCoin.h"
 
+/**
+ * @brief Класс Ethereum, наследующий от BasicCoin.
+ * 
+ * Этот класс представляет криптовалюту Ethereum и наследует основные свойства и функциональность от BasicCoin.
+ */
 class Ethereum : public BasicCoin {
 
 public:
+  /**
+   * @brief Конструктор класса Ethereum.
+   * 
+   * @param _id Уникальный идентификатор Ethereum.
+   * @param _isImplemented Флаг, указывающий на то, реализован ли класс Ethereum.
+   */
   Ethereum(unsigned _id, bool _isImplemented)
       : BasicCoin(_id, "ETH", _isImplemented) {
   }
