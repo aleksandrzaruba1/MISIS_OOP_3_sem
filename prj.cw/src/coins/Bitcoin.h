c481e4f4e3fc81946c4bea062e4d3aaf5707fdf7
aleksandrzaruba1 <156068898+aleksandrzaruba1@users.noreply.github.com>
2024-04-12 12:04:14 +0300

    Update Bitcoin.h

--- prj.cw/src/coins/Bitcoin.h
+++ prj.cw/src/coins/Bitcoin.h
@@ -1,10 +1,18 @@
 #pragma once
 #include "BasicCoin.h"
 
+/**
+ * @brief Класс Bitcoin является производным от BasicCoin и представляет криптовалюту Bitcoin.
+ */
 class Bitcoin : public BasicCoin {
-
 public:
-  Bitcoin(unsigned _id, bool _isImplemented)
-      : BasicCoin(_id, "BTC", _isImplemented) {
-  }
+    /**
+     * @brief Конструктор класса Bitcoin.
+     * 
+     * @param _id Идентификатор Bitcoin.
+     * @param _isImplemented Флаг, указывающий, реализован ли класс Bitcoin.
+     */
+    Bitcoin(unsigned _id, bool _isImplemented)
+        : BasicCoin(_id, "BTC", _isImplemented) {
+    }
 };
