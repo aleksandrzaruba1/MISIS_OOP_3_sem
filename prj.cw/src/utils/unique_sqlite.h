2454454d38ac269b23afe3c334d6725c33ef4346
aleksandrzaruba1 <156068898+aleksandrzaruba1@users.noreply.github.com>
2024-04-12 15:54:39 +0300

    Update unique_sqlite.h

--- prj.cw/src/utils/unique_sqlite.h
+++ prj.cw/src/utils/unique_sqlite.h
@@ -2,8 +2,14 @@
 
 #include "sqlite3.h"
 #include <memory>
-
+/**
+ * @brief Функтор для удаления ресурсов SQLite3.
+ */
 struct sqlite_deleter {
+ /**
+   * @brief Оператор удаления для объектов типа sqlite3.
+   * @param[in] S Указатель на объект sqlite3 для закрытия.
+   */
   void operator () (sqlite3 *S) {
     sqlite3_close(S);
   }
